0578ff37180935efa959a691822dce0659beb25e
t514_muiv <t514@muiv.ru>
2024-04-19 16:08:24 +0300

    reboot

---
+++ Enemy.py
@@ -0,0 +1,50 @@
+import pygame
+import random
+from Entity import Entity
+
+
+W = 1280
+H = 720
+screen = pygame.display.set_mode((W, H))
+
+ground_image = pygame.image.load('images/Ground.png')
+GROUND_H = ground_image.get_height()
+
+enemy_dead_image = pygame.image.load('images/enemy/DEAD ENEMY RIGHT.png').convert_alpha()
+enemy_dead_image = pygame.transform.scale(enemy_dead_image, (200, 50))
+
+#Враг
+enemy_dead_image = pygame.image.load('images/enemy/DEAD ENEMY LEFT.png').convert_alpha()
+enemy_dead_image = pygame.transform.scale(enemy_dead_image, (200, 50))
+
+#Враг
+enemy_image = pygame.image.load('images/enemy/ENEMY RIGHT.png').convert_alpha()
+enemy_image = pygame.transform.scale(enemy_image, (200, 100))
+
+player_dead_image = pygame.image.load('images/Player/DEAD Player 2.png').convert_alpha()
+player_dead_image = pygame.transform.scale(player_dead_image, (180, 222))
+
+#Игрок
+player_image = pygame.image.load('images/Player test R/Player 2.png').convert_alpha()
+player_image = pygame.transform.scale(player_image, (180, 222))
+
+
+class Enemy(Entity):
+    def __init__(self):
+        super().__init__(enemy_image)
+        self.spawn()
+
+    def spawn(self):
+        direction = random.randint(0, 1)
+
+        if direction == 0:
+            self.x_speed = self.speed
+            self.rect.bottomright = (0, 0)
+        else:
+            self.x_speed = -self.speed
+            self.rect.bottomleft = (W, 0)
+
+    def update(self):
+        super().update()
+        if self.x_speed > 0 and self.rect.left > W or self.x_speed < 0 and self.rect.right < 0:
+            self.is_out = True
---
+++ Loading.py
@@ -0,0 +1,54 @@
+import pygame, sys, threading
+
+pygame.init()
+
+# Экран и Шрифт
+screen = pygame.display.set_mode((1280, 720))
+W = 1280
+H = 720
+pygame.display.set_caption('Gorilla: The Legacy of the Berserker')
+
+FONT = pygame.font.SysFont("8-bit Arcade In", 100)
+WORK = 1000000
+CLOCK = pygame.time.Clock()
+
+
+class LoadingScreen:
+    def __init__(self, screen):
+        self.screen = screen
+        self.loading_bg = pygame.image.load("images/loading Bar Background.png")
+        self.loading_bg_rect = self.loading_bg.get_rect(center=(W // 2, H // 2))
+
+        self.loading_bar = pygame.image.load("images/Loading Bar.png")
+        self.loading_bar_rect = self.loading_bar.get_rect(midleft=(280, H // 2))
+        self.loading_finished = False
+        self.loading_progress = 0
+        self.loading_bar_width = 8
+        self.work = WORK
+
+        # Запуск потока для выполнения работы
+        threading.Thread(target=self.do_work).start()
+
+    def do_work(self):
+        for i in range(self.work):
+            math_equation = 523687 / 789456 * 89456
+            self.loading_progress = i
+        self.loading_finished = True
+
+    def update(self):
+        if not self.loading_finished:
+            self.loading_bar_width = self.loading_progress / self.work * 720
+            self.loading_bar = pygame.transform.scale(self.loading_bar, (int(self.loading_bar_width), 150))
+            self.loading_bar_rect = self.loading_bar.get_rect(midleft=(280, H // 2))
+
+    def draw(self):
+        self.screen.fill("#0d0e2e")
+        self.screen.blit(self.loading_bg, self.loading_bg_rect)
+        self.screen.blit(self.loading_bar, self.loading_bar_rect)
+        if self.loading_finished:
+            finished = FONT.render("Press BACKSPACE", True, "white")
+            finished_rect = finished.get_rect(center=(W // 2, H // 2))
+            self.screen.blit(finished, finished_rect)
+        pygame.display.update()
+
+        CLOCK.tick(60)
